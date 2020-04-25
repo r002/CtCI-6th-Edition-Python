f5ef4e2799ba6e9576f7a3dc6697462c1a11d1f5
Robert Lin <rcl2106@gmail.com>
2020-04-24 23:30:21 -0400

    Add p01: 'From a list, extract the second largest number.'

--- Chapter1/1_Is Unique/IsUnique.py
+++ Chapter1/1_Is Unique/IsUnique.py
@@ -1,4 +1,8 @@
 # O(N)
+# 1.1 Is Unique: Implement an algorithm to determine if a string has all
+# unique characters. What if you cannot use additional data structures?
+# Hints: #44, #117, #132
+
 import unittest
 
 
